ad65faf2a6930fa94688029a5c75501fa50a289f
jlferrete <joseluisferrete@gmail.com>
2023-07-31 22:41:21 +0200

    Created server, root route and 404

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 // Ejercicio 1: crear servidor HTTP con Node
+
+const http = require('node:http') // protocolo HTTP
+const fs = require('node:fs')
+
 function startServer () {
-  // return server
+
+  const desiredPort = process.env.PORT ?? 1234
+
+  const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+    if (req.url === '/') {
+      res.end('<h1>¡Hola mundo!</h1>')
+    } else {
+      res.statusCode = 404 // Not Found
+      res.end('<h1>404</h1>')
+    }
+  }
+
+  const server = http.createServer(processRequest)
+
+  server.listen(desiredPort, () => {
+    console.log(`server listening on port http://localhost:${desiredPort}`)
+  })
+
+  return server
+
 }
+
 
 module.exports = {
   startServer
