d6030fa4dde19ce7d042a6d9afc53bfe18fe33ef
jlferrete <joseluisferrete@gmail.com>
2023-08-03 17:18:40 +0200

    Refactor to clarify code

--- index.js
+++ index.js
@@ -3,44 +3,33 @@
 const http = require('node:http') // protocolo HTTP
 const fs = require('node:fs')
 const path = require('node:path')
+const PORT = process.env.PORT ?? 1234
 
+function err404(req, res) {
+  res.writeHead(404, {
+    "Content-Type": "text/html; charset=utf8",
+  });
+  return res.end("<h1>404</h1>");
+}
 
-function startServer () {
+function err405(req, res) {
+  res.writeHead(405, {
+    "Content-Type": "text/html; charset=utf8",
+  });
+  return res.end("<h1>405</h1>");
+}
 
-  const desiredPort = process.env.PORT ?? 1234
-
-  const processRequest = (req, res) => {
-    
-
-    if (req.url === '/') {
-      res.setHeader('Content-Type', 'text/html; charset=utf-8')
-      res.end('<h1>¡Hola mundo!</h1>')
-    } else if (req.url === '/logo.webp') {
-      fs.readFile(path.resolve(__dirname, './assets/logo.webp'), (err, data) => {
-        if (err) {         
-          res.statusCode = 500
-          res.end('<h1>500 Internal Server Error</h1>')
-        } else {
-          res.setHeader('Content-Type', 'image/webp')
-          res.end(data)
-        }
-      })
-    } else {
-      res.statusCode = 404 // Not Found
-      res.end('<h1>404</h1>')
-    }
-  }
-
-  const server = http.createServer(processRequest)
-
-  server.listen(desiredPort, () => {
-    console.log(`server listening on port http://localhost:${desiredPort}`)
-  })
-
-  return server
+function processRequest(req, res) {
+  const { method, url } = req;
 
 }
 
+function startServer () {
+  const server = http.createServer(processRequest);
+  return server.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  });
+}
 
 module.exports = {
   startServer
