ed563fe84842d2b20909b451ac3a608f576e7b1f
jlferrete <joseluisferrete@gmail.com>
2023-08-03 23:47:02 +0200

    Add processRequest

--- index.js
+++ index.js
@@ -21,7 +21,54 @@ function err405(req, res) {
 
 function processRequest(req, res) {
   const { method, url } = req;
+  switch (url) {
+    case "/":
+      if (method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html; charset:utf8" });
+        return res.end("<h1>¡Hola mundo!</h1>");
+      } else {
+        return err405(req, res);
+      }
+    case "/404":
+      if (method === "GET") {
+        return err404(req, res);
+      } else {
+        return err405(req, res);
+      }
 
+    case "/logo.webp":
+      if (method === "GET") {
+        fs.readFile(path.join("assets", "logo.webp"), (err, data) => {
+          if (err) {
+            console.log(`Ha ocurrido un error : ${err}`);
+            return;
+          } else {
+            res.writeHead(200, { "Content-Type": "image/webp" });
+            return res.end(data);
+          }
+        });
+      } else {
+        return err405(req, res);
+      }
+      break;
+    case "/contacto":
+      if (method === "POST") {
+        let body = "";
+        req.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+        req.on("end", () => {
+          const data = JSON.parse(body);
+          res.writeHead(201, { "Content-Type": "application/json" });
+          return res.end(JSON.stringify(data));
+        });
+      } else {
+        return err405(req, res);
+      }
+      break;
+    default:
+      err404(req, res);
+  }
 }
 
 function startServer () {
